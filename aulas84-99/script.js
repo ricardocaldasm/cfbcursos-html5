59c8f19066d98ed0a2bcf64db37b0677d5502955
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2023-05-11 12:37:21 -0300

    at

---
+++ aulas84-99/script.js
@@ -0,0 +1,20 @@
+const elementos=document.querySelectorAll('[data-anima]');
+const animacaoClass='animacao';
+
+function animaScroll() {
+    const topoPaginaNaJanela=window.scrollY+((window.innerHeight*3)/4);
+    elementos.forEach(function(elemento){
+        if(topoPaginaNaJanela > elemento.offsetTop){
+            elemento.classList.add(animacaoClass);
+        }else{
+            elemento.classList.remove(animacaoClass);
+        }
+    });
+
+}
+
+if(elementos.length){
+    window.addEventListener('scroll',function(){
+        animaScroll();
+    })
+}
